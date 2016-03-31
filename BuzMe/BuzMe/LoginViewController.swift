d3de3922974ec4b6161a5702e342a1654cd0e88e
anitaleung <anita.wk.leung@gmail.com>
2016-03-30 22:06:28 -0700

    populate login view controller

---
+++ BuzMe/BuzMe/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  BuzMe
+//
+//  Created by Anita on 3/30/16.
+//  Copyright © 2016 BuzMe-codepath. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                print("you're logged in!")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(sender: AnyObject) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackgroundWithBlock { (
+            success: Bool, error: NSError?) -> Void in
+            if success {
+                print("Yay, created a user!")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription)
+                if error?.code == 202 {
+                    print("User name is taken")
+                }
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
