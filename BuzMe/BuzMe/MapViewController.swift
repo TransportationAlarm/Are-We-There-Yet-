c568b24513f32ab8a438b7c63b1497236872accc
anitaleung <anita.wk.leung@gmail.com>
2016-04-07 19:13:52 -0700

    add marker

--- BuzMe/BuzMe/MapViewController.swift
+++ BuzMe/BuzMe/MapViewController.swift
@@ -34,28 +34,32 @@ class MapViewController: UIViewController {
             if let placeLikelihoodList = placeLikelihoodList {
                 let place = placeLikelihoodList.likelihoods.first?.place
                 if let place = place {
-                    print(place.name)
-                    print(place.formattedAddress)
-                    self.nameLabel.text = place.name
-                    print(place.coordinate)
-                    self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ")
-                        .joinWithSeparator("\n")
+//                    print(place.name)
+//                    print(place.formattedAddress)
+//                    self.nameLabel.text = place.name
+//                    print(place.coordinate)
+//                    self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ")
+//                        .joinWithSeparator("\n")
+                    let lat = place.coordinate.latitude
+                    let long = place.coordinate.longitude
                     
-                    let camera = GMSCameraPosition.cameraWithLatitude(place.coordinate.latitude,
-                        longitude: place.coordinate.longitude, zoom: 6)
+                    let camera = GMSCameraPosition.cameraWithLatitude(lat,
+                        longitude: long, zoom: 6)
                     let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
                     mapView.myLocationEnabled = true
                     self.view = mapView
+                    
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2DMake(lat, long)
+//                    marker.title = "Sydney"
+//                    marker.snippet = "Australia"
+                    marker.map = mapView
                 }
             }
         })
 
         
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
+        
     }
     
     // Add a UIButton in Interface Builder to call this function
