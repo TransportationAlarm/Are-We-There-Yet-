54b14d8d0aaef305ac39e0f1dbe4365ce47f71db
anitaleung <anita.wk.leung@gmail.com>
2016-04-07 18:04:45 -0700

    attempt at getting current location

--- BuzMe/BuzMe/MapViewController.swift
+++ BuzMe/BuzMe/MapViewController.swift
@@ -10,21 +10,72 @@ import UIKit
 import GoogleMaps
 
 class MapViewController: UIViewController {
+    
+    var placesClient: GMSPlacesClient?
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-                                                          longitude: 151.20, zoom: 6)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view = mapView
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        placesClient = GMSPlacesClient()
+
+//        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+//                                                          longitude: 151.20, zoom: 6)
+//        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+//        mapView.myLocationEnabled = true
+//        self.view = mapView
+//        
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+    }
+    
+    // Add a UIButton in Interface Builder to call this function
+    @IBAction func getCurrentPlace(sender: UIButton) {
+        
+        placesClient?.currentPlaceWithCallback({
+            (placeLikelihoodList: GMSPlaceLikelihoodList?, error: NSError?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress!.componentsSeparatedByString(", ")
+                        .joinWithSeparator("\n")
+                }
+            }
+        })
+    }
+    
+    // get current location
+    @IBAction func onCurrLocationBtn(sender: AnyObject) {
+        placesClient!.currentPlaceWithCallback({ (placeLikelihoods, error) -> Void in
+            guard error == nil else {
+                print("Current Place error: \(error!.localizedDescription)")
+                return
+            }
+            
+            if let placeLikelihoods = placeLikelihoods {
+                for likelihood in placeLikelihoods.likelihoods {
+                    let place = likelihood.place
+                    print("Current Place name \(place.name) at likelihood \(likelihood.likelihood)")
+                    print("Current Place address \(place.formattedAddress)")
+                    print("Current Place attributions \(place.attributions)")
+                    print("Current PlaceID \(place.placeID)")
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
