71b19bc0ffb726bc0716e2ddcb0869ce25d809fb
anitaleung <anita.wk.leung@gmail.com>
2016-04-14 19:15:39 -0700

    remove polylines

--- BuzMe/BuzMe/MapViewController.swift
+++ BuzMe/BuzMe/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SRKUtility
 import GoogleMaps
 
 class MapViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
@@ -23,7 +24,7 @@ class MapViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
         super.viewDidLoad()
 
         placesClient = GMSPlacesClient()
-        getCurrentLocation()
+        //getCurrentLocation()
         
 
 
@@ -41,7 +42,65 @@ class MapViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
         googleMapsView.myLocationEnabled = true
         googleMapsView.settings.myLocationButton = true
         
-        getCurrentLocation()
+        //getCurrentLocation()
+        
+        // draw route
+        // let strHome = SRKUtility.addURLEncoding("Banana Republic, Grant Avenue, San Francisco, CA")
+        // let strOffice = SRKUtility.addURLEncoding("Union Square, San Francisco, CA")
+//        let strOrigin = "37.7905153,-122.4099926"
+//        let strDestination = "37.7890994,-122.4100355"
+//        let str = "https://maps.googleapis.com/maps/api/directions/json?origin=\(strOrigin)&destination=\(strDestination)&key=AIzaSyAROhx7BpyklgsThy6g-SpAtZxqnVgHX8I"
+//        print(str)
+//
+//        SRKUtility.invokeRequestForJSON(NSMutableURLRequest(URL: NSURL(string: str)!)) { (obj:AnyObject?, error:NSString?) -> Void in
+//            if let r = error {
+//                print("Error occured \(r)")
+//            } else if let objD = obj as? NSDictionary {
+//                print("Response in dictionary form \(objD)")
+//                self.parseAndDisplayDirection(objD)
+//            } else if let objA = obj as? NSArray {
+//                print("Response in array form \(objA)")
+//            }
+//        }
+    }
+    
+    func parseAndDisplayDirection(objD:NSDictionary) {
+        if let routes = objD.valueForKey("routes") as? NSArray {
+            if let routesD = routes.objectAtIndex(0) as? NSDictionary {
+                
+                if let legs = routesD.valueForKey("legs") as? NSArray {
+                    if let legs0 = legs.objectAtIndex(0) as? NSDictionary {
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            let endAddress = legs0.valueForKey("end_address") as! String
+                            let startAddress = legs0.valueForKey("start_address") as! String
+                            let startLoc = legs0.valueForKey("start_location") as! NSDictionary
+                            let endLoc = legs0.valueForKey("end_location") as! NSDictionary
+                            let startCoOd = CLLocationCoordinate2D(latitude: startLoc.valueForKey("lat") as! Double, longitude: startLoc.valueForKey("lng") as! Double)
+                            let endCoOd = CLLocationCoordinate2D(latitude: endLoc.valueForKey("lat") as! Double, longitude: endLoc.valueForKey("lng") as! Double)
+                            
+                            let startMarker = GMSMarker(position: startCoOd)
+                            let endMarker = GMSMarker(position: endCoOd)
+                            startMarker.title = startAddress.componentsSeparatedByString(",")[0]
+                            endMarker.title = endAddress.componentsSeparatedByString(",")[0]
+                            startMarker.map = self.googleMapsView
+                            endMarker.map = self.googleMapsView
+                            let bounds = GMSCoordinateBounds(coordinate: startCoOd, coordinate: endCoOd)
+                            self.googleMapsView?.animateWithCameraUpdate(GMSCameraUpdate.fitBounds(bounds))
+                        })
+                    }
+                }
+                
+                if let polyLine = routesD.valueForKey("overview_polyline") as? NSDictionary {
+                    if let points = polyLine.valueForKey("points") as? String {
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            let path = GMSPath(fromEncodedPath: points)
+                            let line = GMSPolyline(path: path)
+                            line.map = self.googleMapsView
+                        })
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func searchWithAddress(sender: AnyObject) {
@@ -59,7 +118,7 @@ class MapViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
             let position = CLLocationCoordinate2DMake(lat, long)
             let marker = GMSMarker(position: position)
             
-            let camera = GMSCameraPosition.cameraWithLatitude(lat, longitude: long, zoom: 6)
+            let camera = GMSCameraPosition.cameraWithLatitude(lat, longitude: long, zoom: 16)
             self.googleMapsView.camera = camera
             
             marker.title = title
---
+++ BuzMe/Pods/Target Support Files/SRKUtility/SRKUtility-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SRKUtilityVersionNumber;
+FOUNDATION_EXPORT const unsigned char SRKUtilityVersionString[];
+
